5f99891936827e889c135dd65b30c726e15e53a8
Dimas Capelari <97951537+dimascapelari@users.noreply.github.com>
2023-01-22 16:40:35 -0300

    Menu Lateral - Drawer

---
+++ src/shared/components/menu-lateral/MenuLateral.tsx
@@ -0,0 +1,58 @@
+import ListItemButton from "@mui/material/ListItemButton";
+import List from "@mui/material/List";
+import {
+  Avatar,
+  Divider,
+  Drawer,
+  Icon,
+  ListItemIcon,
+  ListItemText,
+  useTheme,
+} from "@mui/material";
+import { Box } from "@mui/system";
+import { PropsWithChildren } from "react";
+
+export const MenuLateral = ({ children }: PropsWithChildren<{}>) => {
+  const theme = useTheme();
+
+  return (
+    <>
+      <Drawer variant="permanent">
+        <Box
+          width={theme.spacing(28)}
+          height="100%"
+          display="flex"
+          flexDirection="column"
+        >
+          <Box
+            width="100%"
+            height={theme.spacing(20)}
+            display="flex"
+            alignItems="center"
+            justifyContent="center"
+          >
+            <Avatar
+              sx={{ height: theme.spacing(12), width: theme.spacing(12) }}
+              src="https://avatars.githubusercontent.com/u/97951537?s=400&u=4f8d3699bd0f3f0a7a3b270229f828c1439ee508&v=4"
+            />
+          </Box>
+          <Divider />
+          <Box flex={1}>
+            <List component="nav">
+              <ListItemButton>
+                <ListItemIcon>
+                  <Icon>home</Icon>
+                </ListItemIcon>
+                <ListItemText primary="Página inicial" />
+              </ListItemButton>
+            </List>
+          </Box>
+        </Box>
+      </Drawer>
+
+      <Box height="100vh" marginLeft={theme.spacing(28)}>
+        {children}
+      </Box>
+    </>
+  );
+};
